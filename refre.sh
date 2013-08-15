0de8fe564366e01e8fe0bc7e9286ee5d9bc6f7e7
Oliver Gondža <ogondza@redhat.com>
2013-08-15 10:39:45 +0200

    init

---
+++ refre.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "No update_center_id provided" >&2
+  echo "Usage: $0 <update_center_id>" >&2
+  exit 1
+fi
+
+cd /opt/update-center/
+
+rm -r /var/www/$1/*
+
+/opt/apache-maven-3.0.5/bin/mvn -e exec:java -Dexec.args="\
+    -id $1\
+    -hpiDirectory /var/opt/update-center/$1/\
+    -key /etc/pki/tls/private/update-center.key\
+    -certificate /etc/pki/tls/certs/update-center.crt\
+    -root-certificate /etc/pki/tls/certs/update-center.crt\
+    -www /var/www/$1\
+    -download /var/www/$1/download\
+    -repository http://ogondza01.lab.eng.brq.redhat.com/$1\
+    -includeSnapshots\
+    -pretty"
+
