c8c83215a7601ce71f54a330d4c456f4cfaea215
Oliver Gondža <ogondza@redhat.com>
2014-07-18 11:58:13 +0200

    Introduce remove.sh

---
+++ remove.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Remove plugins from update center. This is destructive operation.
+# Ex.: ./grab.sh jenkins-test git-plugin
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <update_center_id> <artifact_id...>" >&2
+  exit 1
+fi
+
+data_dir=/var/opt/update-center/$1
+
+shift
+
+for artifact in "$@"; do
+  rm -v $data_dir/$artifact.[jh]p[il]*
+done
