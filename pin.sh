0e9bed3209996a9a31fb24ee00d7e086de0924a1
Oliver Gondža <ogondza@gmail.com>
2015-07-23 17:01:57 +0200

    Support pin.sh

---
+++ pin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Pin plugins in the update center
+# Ex.: ./pin.sh jenkins-test junit
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <update_center_id> <artifact_id...>" >&2
+  exit 1
+fi
+
+data_dir=/var/opt/update-center/$1
+
+shift
+
+for artifact in "$@"; do
+  for file in $(ls $data_dir/$artifact.[jh]pi*); do
+    touch "$file.pinned"
+    echo "$file.pinned" >&2
+  done
+done
