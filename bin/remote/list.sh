54e97c1c1227a84c4fc9b0ff19fef01c1d994529
Oliver Gondža <ogondza@gmail.com>
2015-11-23 10:25:49 +0100

    Use library function to show list

--- bin/remote/list.sh
+++ bin/remote/list.sh
@@ -5,9 +5,4 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-data_dir=$(update_center_dir "$1")
-
-for plugin in $data_dir/*.[jh]pi; do
-  name=$(basename $plugin)
-  echo "${name%%.*} $(plugin_version $plugin)"
-done
+list_plugins_in_uc "$1"
