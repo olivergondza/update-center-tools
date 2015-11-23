9bb435761489fb0be961d2992edf955c41fc2431
Oliver Gondža <ogondza@gmail.com>
2015-11-23 10:31:08 +0100

    Fix #1: Do not pin .pinned files

--- bin/remote/pin.sh
+++ bin/remote/pin.sh
@@ -11,7 +11,7 @@ data_dir=$(update_center_dir $1)
 shift
 
 for artifact in "$@"; do
-  for file in $(ls $data_dir/$artifact.[jh]pi*); do
+  for file in $(ls $data_dir/$artifact.[jh]pi); do
     touch "$file.pinned"
     echo "$file.pinned" >&2
   done
