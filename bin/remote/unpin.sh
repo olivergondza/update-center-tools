68e887d7344149a16ce57f8c5f0d0c02237f7e72
Oliver Gondža <ogondza@gmail.com>
2015-11-23 10:32:58 +0100

    Introduce unpin

---
+++ bin/remote/unpin.sh
@@ -0,0 +1,17 @@
+# Unpin plugins in the update center
+# Ex.: ./unpin.sh jenkins-test junit
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <update_center_id> <artifact_id...>" >&2
+  exit 1
+fi
+
+data_dir=$(update_center_dir $1)
+
+shift
+
+for artifact in "$@"; do
+  for file in $(ls $data_dir/$artifact.[jh]pi); do
+    rm -f "$file.pinned"
+  done
+done
