40caead8f698990091d15c132c93eee24779036c
Oliver Gondža <ogondza@gmail.com>
2016-04-28 11:59:34 +0200

    Introduce copy-collection

---
+++ bin/remote/copy-collection.sh
@@ -0,0 +1,21 @@
+# Duplicate existing collection
+
+if [ $# -ne 2 ]; then
+  command_usage "copy-sollection <src_update_center_id> <new_update_center_id>" >&2
+  exit 1
+fi
+
+src="/var/opt/update-center/$1"
+if [ ! -d $src ]; then
+  echo "$src does not exist" >&2
+  exit 1
+fi
+
+dst="/var/opt/update-center/$2"
+if [ -d $dst ]; then
+  echo "$dst exists already" >&2
+  exit 1
+fi
+
+set -x
+cp -r "$src" "$dst"
